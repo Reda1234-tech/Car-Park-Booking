a3bf9fae1dc3584d789af9059195dc5a7ab52e08
= <reda.badry@stu.eng.aswu.edu.eg>
2025-02-02 01:07:57 +0200

    add slots from db

--- lib/parking_booking_page_copy.dart
+++ lib/parking_booking_page_copy.dart
@@ -2,9 +2,9 @@ import 'package:car_park_booking/maps.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import './main.dart';
-import 'package:flutter/services.dart';
-
+import 'package:flutter/services.dart' show Color, FilteringTextInputFormatter, FontWeight, LengthLimitingTextInputFormatter, Size, TextAlign, TextEditingValue, TextInputFormatter, TextInputType;
+import './book.dart';
+import 'main.dart';
 // import 'package:gif/gif.dart';
 
 // void main() {
